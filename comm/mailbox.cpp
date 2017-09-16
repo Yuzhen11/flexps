6162953ac19e11faa6a6f865eb912f7e3aee0c05
Zhanhao (Jasper) Liu <jasperlau95@gmail.com>
2017-09-16 17:22:36 +0800

    [Finished] Add Send and Recv unit test (#8)

--- comm/mailbox.cpp
+++ comm/mailbox.cpp
@@ -46,10 +46,23 @@ void Mailbox::Stop() {
   exit_msg.meta.flag = Flag::kExit;
   Send(exit_msg);
   receiver_thread_.join();
+
   // Kill all the registered threads
   for (auto& queue : queue_map_) {
     queue.second->Push(exit_msg);
   }
+
+  // close sockets
+  int linger = 0;
+  int rc = zmq_setsockopt(receiver_, ZMQ_LINGER, &linger, sizeof(linger));
+  CHECK(rc == 0 || errno == ETERM);
+  CHECK_EQ(zmq_close(receiver_), 0);
+  for (auto& it : senders_) {
+    int rc = zmq_setsockopt(it.second, ZMQ_LINGER, &linger, sizeof(linger));
+    CHECK(rc == 0 || errno == ETERM);
+    CHECK_EQ(zmq_close(it.second), 0);
+  }
+  zmq_ctx_destroy(context_);
 }
 
 void Mailbox::Connect(const Node& node) {
