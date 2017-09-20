a5fb244e41df19e0799959e74278c86d22b5ecb1
Zhanhao (Jasper) Liu <jasperlau95@gmail.com>
2017-09-20 12:33:42 +0800

    Improve worker_spec and add tests (#11)

--- driver/worker_spec.hpp
+++ driver/worker_spec.hpp
@@ -5,12 +5,10 @@
 #include <map>
 #include <vector>
 
-#include "glog/logging.h"
-
 namespace flexps {
 
 /*
- * node_id: 0, 2, 3
+ * node_id: 0, 1
  * worker_id for this task: 0, 1, 2 .. consecutive
  * thread_id: global worker thread id
  */
@@ -18,61 +16,36 @@ class WorkerSpec {
  public:
   // {{0, 3}, {1, 2}}: 3 workers on node 0, 2 workers on node 1.
   WorkerSpec() = delete;
-  WorkerSpec(const std::vector<WorkerAlloc>& worker_alloc) {
-    Init(worker_alloc);
-  }
-  bool HasLocalWorkers(uint32_t node_id) const {
-    return node_to_workers_.find(node_id) != node_to_workers_.end();
-  }
-  const std::vector<uint32_t>& GetLocalWorkers(uint32_t node_id) const {
-    auto it = node_to_workers_.find(node_id);
-    CHECK(it != node_to_workers_.end());
-    return it->second;
-  }
-  const std::vector<uint32_t>& GetLocalThreads(uint32_t node_id) const {
-    auto it = node_to_threads_.find(node_id);
-    CHECK(it != node_to_threads_.end());
-    return it->second;
-  }
+  WorkerSpec(const std::vector<WorkerAlloc>& worker_alloc);
+  bool HasLocalWorkers(uint32_t node_id) const;
+  const std::vector<uint32_t>& GetLocalWorkers(uint32_t node_id) const;
+  const std::vector<uint32_t>& GetLocalThreads(uint32_t node_id) const;
 
-  std::map<uint32_t, std::vector<uint32_t>> GetNodeToWorkers() {
-    return node_to_workers_;
-  }
+  std::map<uint32_t, std::vector<uint32_t>> GetNodeToWorkers();
+  std::vector<uint32_t> GetThreadIds();
 
-  std::vector<uint32_t> GetThreadIds() {
-    return {thread_ids_.begin(), thread_ids_.end()};
-  }
+  void InsertWorkerIdThreadId(uint32_t worker_id, uint32_t thread_id);
 
-  void InsertWorkerIdThreadId(uint32_t worker_id, uint32_t thread_id) {
-    CHECK(worker_to_thread_.find(worker_id) == worker_to_thread_.end());
-    CHECK(thread_to_worker_.find(thread_id) == thread_to_worker_.end());
-    CHECK(thread_ids_.find(thread_id) == thread_ids_.end());
-    CHECK(worker_to_node_.find(worker_id) != worker_to_node_.end());
-    worker_to_thread_[worker_id] = thread_id;
-    thread_to_worker_[thread_id] = worker_id;
-    thread_ids_.insert(thread_id);
-    node_to_threads_[worker_to_node_[worker_id]].push_back(thread_id);
-  }
+  static const uint32_t kMaxNodeId = 1000;
+  static const uint32_t kMaxThreadsPerNode = 1000;
+  // BgThreads include server threads, worker helper threads and model init threads.
+  static const uint32_t kMaxBgThreadsPerNode = 100;
+
  private:
-  void Init(const std::vector<WorkerAlloc>& worker_alloc) {
-    uint32_t worker_count = 0;
-    for (const auto& worker : worker_alloc) {
-      for (int i = 0; i < worker.num_workers; ++ i) {
-        worker_to_node_[num_workers] = worker.node_id;
-        node_to_workers_[worker.node_id].push_back(num_workers);
-        num_workers += 1;
-      }
-    }
-  }
+  void Init(const std::vector<WorkerAlloc>& worker_alloc);
 
   // {0,0}, {1,0}, {2,0}, {3,1}, {4,1}
   std::map<uint32_t, uint32_t> worker_to_node_;
   // {0, {0,1,2}}, {1, {3,4}}
   std::map<uint32_t, std::vector<uint32_t>> node_to_workers_;
 
+  // {0, 100}, {1, 101}, {2, 102}, {3, 1100}, {4, 1101}
   std::map<uint32_t, uint32_t> worker_to_thread_;
   std::map<uint32_t, uint32_t> thread_to_worker_;
+
+  // {0,100}, {0,101}, {0,102}, {1,1100}, {1,1101}
   std::map<uint32_t, std::vector<uint32_t>> node_to_threads_;
+  // {100, 101, 102, 1100, 1101}
   std::set<uint32_t> thread_ids_;
   uint32_t num_workers = 0;
 };
