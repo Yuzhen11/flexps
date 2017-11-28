90a333325958863d15b5212e84fff2bc48313687
Yuzhen11 <huangyuzhen93@gmail.com>
2017-11-28 23:46:48 +0800

    Add channel_example, extend simple_id_mapper to support channel

--- driver/simple_id_mapper_test.cpp
+++ driver/simple_id_mapper_test.cpp
@@ -62,5 +62,30 @@ TEST_F(TestSimpleIdMapper, AllocateDeallocateThread) {
   EXPECT_EQ(id_mapper.GetNodeIdForThread(SimpleIdMapper::kMaxThreadsPerNode + SimpleIdMapper::kMaxBgThreadsPerNode), 1);
 }
 
+TEST_F(TestSimpleIdMapper, GetChannelThreads) {
+  Node n1{0, "worker1", 12352};
+  Node n2{1, "worker1", 12353};
+  Node n3{3, "worker1", 12354};
+  SimpleIdMapper id_mapper(n2, {n1, n2, n3});
+  const uint32_t num_local_threads = 2;
+  const uint32_t num_global_threads = num_local_threads * 3;
+  auto ret = id_mapper.GetChannelThreads(num_local_threads, num_global_threads);
+  
+  std::pair<std::vector<uint32_t>, std::unordered_map<uint32_t, uint32_t>> expected_ret;
+  expected_ret.second.insert({0, SimpleIdMapper::kChannelThreadId});
+  expected_ret.second.insert({1, SimpleIdMapper::kChannelThreadId+1});
+  expected_ret.second.insert({2, SimpleIdMapper::kMaxThreadsPerNode+SimpleIdMapper::kChannelThreadId});
+  expected_ret.second.insert({3, SimpleIdMapper::kMaxThreadsPerNode+SimpleIdMapper::kChannelThreadId+1});
+  expected_ret.second.insert({4, 3*SimpleIdMapper::kMaxThreadsPerNode+SimpleIdMapper::kChannelThreadId});
+  expected_ret.second.insert({5, 3*SimpleIdMapper::kMaxThreadsPerNode+SimpleIdMapper::kChannelThreadId+1});
+  expected_ret.first = {2, 3};
+  EXPECT_EQ(ret.first, expected_ret.first);
+  ASSERT_EQ(ret.second.size(), expected_ret.second.size());
+  for (auto& kv: ret.second) {
+    EXPECT_EQ(kv.second, expected_ret.second[kv.first]);
+  }
+  id_mapper.ReleaseChannelThreads();
+}
+
 }  // namespace
 }  // namespace flexps
