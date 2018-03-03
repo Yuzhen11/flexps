ff7be1210994575228ddf0c75ee934ea39533424
Zizinc <kuchikitouko@hotmail.com>
2018-03-03 01:07:41 -0800

    1. Delete gbdt data directory 2. Add Vector, Map in sarray_binstream 3. Include <numeric> in kvstore_performance_example.cpp (iota not found error)

--- base/sarray_binstream.hpp
+++ base/sarray_binstream.hpp
@@ -44,4 +44,71 @@ SArrayBinStream& operator>>(SArrayBinStream& bin, T& t) {
   return bin;
 }
 
+template <typename InputT>
+SArrayBinStream& operator<<(SArrayBinStream& stream, const std::basic_string<InputT>& v) {
+    size_t len = v.size();
+    stream << len;
+    for (auto& elem : v)
+        stream << elem;
+    return stream;
+}
+
+template <typename OutputT>
+SArrayBinStream& operator>>(SArrayBinStream& stream, std::basic_string<OutputT>& v) {
+    size_t len;
+    stream >> len;
+    v.clear();
+    try {
+        v.resize(len);
+    } catch (std::exception e) {
+        assert(false);
+    }
+    for (auto& elem : v)
+        stream >> elem;
+    return stream;
+}
+
+
+template <typename K, typename V>
+SArrayBinStream& operator<<(SArrayBinStream& stream, const std::map<K, V>& map) {
+    size_t len = map.size();
+    stream << len;
+    for (auto& elem : map)
+        stream << elem;
+    return stream;
+}
+
+template <typename K, typename V>
+SArrayBinStream& operator>>(SArrayBinStream& stream, std::map<K, V>& map) {
+    size_t len;
+    stream >> len;
+    map.clear();
+    for (int i = 0; i < len; i++) {
+        std::pair<K, V> elem;
+        stream >> elem;
+        map.insert(elem);
+    }
+    return stream;
+}
+
+template <typename InputT>
+SArrayBinStream& operator<<(SArrayBinStream& stream, const std::vector<InputT>& v) {
+    size_t len = v.size();
+    stream << len;
+    for (int i = 0; i < v.size(); ++i)
+        stream << v[i];
+    return stream;
+}
+
+template <typename OutputT>
+SArrayBinStream& operator>>(SArrayBinStream& stream, std::vector<OutputT>& v) {
+    size_t len;
+    stream >> len;
+    v.clear();
+    v.resize(len);
+    for (int i = 0; i < v.size(); ++i)
+        stream >> v[i];
+    return stream;
+}
+
 }  // namespace flexps
