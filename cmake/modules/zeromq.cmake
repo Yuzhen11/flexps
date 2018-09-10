ecebff6c14d1480a14bc2795f532e6822117507f
Yuzhen11 <huangyuzhen93@gmail.com>
2018-09-10 10:54:59 +0800

    update cmake zeromq

--- cmake/modules/zeromq.cmake
+++ cmake/modules/zeromq.cmake
@@ -31,31 +31,27 @@ else(ZMQ_FOUND)
     message (STATUS "ZeroMQ will be included as a third party:")
     include(ExternalProject)
     set(THIRDPARTY_DIR ${PROJECT_BINARY_DIR}/third_party)
-    if(NOT ZMQ_INCLUDE_DIR)
-        ExternalProject_Add(
-            cppzmq
-            GIT_REPOSITORY "https://github.com/zeromq/cppzmq"
-            PREFIX ${THIRDPARTY_DIR}
-            UPDATE_COMMAND ""
-            CONFIGURE_COMMAND ""
-            BUILD_COMMAND ""
-            INSTALL_COMMAND cp ${THIRDPARTY_DIR}/src/cppzmq/zmq.hpp ${PROJECT_BINARY_DIR}/include/zmq.hpp
-        )
-        list(APPEND external_project_dependencies cppzmq)
-        add_dependencies(cppzmq libzmq)
-    endif(NOT ZMQ_INCLUDE_DIR)
-    if(NOT ZMQ_LIBRARY)
-        ExternalProject_Add(
-            libzmq
-            GIT_REPOSITORY "https://github.com/zeromq/libzmq"
-            GIT_TAG "v4.2.1"
-            PREFIX ${THIRDPARTY_DIR}
-            UPDATE_COMMAND ""
-            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_BINARY_DIR}
-            CMAKE_ARGS -DZMQ_BUILD_TESTS=OFF
-        )
-        list(APPEND external_project_dependencies libzmq)
-    endif(NOT ZMQ_LIBRARY)
+    ExternalProject_Add(
+        cppzmq
+        GIT_REPOSITORY "https://github.com/zeromq/cppzmq"
+        PREFIX ${THIRDPARTY_DIR}
+        UPDATE_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND cp ${THIRDPARTY_DIR}/src/cppzmq/zmq.hpp ${PROJECT_BINARY_DIR}/include/zmq.hpp
+    )
+    list(APPEND external_project_dependencies cppzmq)
+    ExternalProject_Add(
+        libzmq
+        GIT_REPOSITORY "https://github.com/zeromq/libzmq"
+        GIT_TAG "v4.2.1"
+        PREFIX ${THIRDPARTY_DIR}
+        UPDATE_COMMAND ""
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${PROJECT_BINARY_DIR}
+        CMAKE_ARGS -DZMQ_BUILD_TESTS=OFF
+    )
+    list(APPEND external_project_dependencies libzmq)
+    add_dependencies(cppzmq libzmq)
     set(ZMQ_INCLUDE_DIR "${PROJECT_BINARY_DIR}/include")
     if(WIN32)
         set(ZMQ_LIBRARY "${PROJECT_BINARY_DIR}/lib/libzmq.lib")
